b0bd6ac0160878375181408403a3cb8e183aeefa
Brenden Lawlor <lawlorbrenden2@gmail.com>
2025-10-08 21:29:43 -0600

    Ported workouts.html code to workouts.jsx

--- src/workouts/workouts.jsx
+++ src/workouts/workouts.jsx
@@ -2,8 +2,52 @@ import React from 'react';
 
 export function Workouts() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Workouts displayed here</div>
+    <main className="container-fluid bg-dark text-light d-flex flex-column align-items-center mt-5 pt-3">
+      <div className="content-wrapper w-75">
+        <h2>My Workout Plan</h2>
+
+        <table className="table table-hover mt-3">
+          <thead>
+            <tr>
+              <th scope="col">Day</th>
+              <th scope="col">Workout Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="table-active"> <td>Monday</td>
+              <td>Push</td>
+            </tr>
+            <tr>
+              <td>Tuesday</td>
+              <td>Pull</td>
+            </tr>
+            <tr>
+              <td>Wednesday</td>
+              <td>Legs</td>
+            </tr>
+            <tr>
+              <td>Thursday</td>
+              <td>Push</td>
+            </tr>
+            <tr>
+              <td>Friday</td>
+              <td>Pull</td>
+            </tr>
+            <tr>
+              <td>Saturday</td>
+              <td>Legs</td>
+            </tr>
+            <tr>
+              <td>Sunday</td>
+              <td>Rest</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div className="exercise-details mt-3 text-light">
+          <p>Click a day to view exercises for that day</p>
+        </div>
+      </div>
     </main>
   );
 }
