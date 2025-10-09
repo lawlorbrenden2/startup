b5aa5085f91c97aaba2a6f132a965605f633a374
Brenden Lawlor <lawlorbrenden2@gmail.com>
2025-10-08 21:14:07 -0600

    added login code

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Login displayed here</div>
+    <main className="container-fluid bg-dark text-light d-flex flex-column align-items-center mt-5 pt-3">
+      <div className="content-wrapper">
+        <h1>Welcome</h1>
+        <form method="get" action="/progress" className="text-center">
+          <div className="input-group mb-3">
+            <span className="input-group-text">@</span>
+            <input className="form-control" type="text" placeholder="your@email.com" />
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text">🔒</span>
+            <input className="form-control" type="password" placeholder="password" />
+          </div>
+          <button type="submit" className="btn btn-primary">Sign in</button>
+          <button type="submit" className="btn btn-dark">Create</button>
+        </form>
+      </div>
     </main>
   );
 }
