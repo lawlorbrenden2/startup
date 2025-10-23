71d073c9824fd1ceeff4a2800ced3a382df22dea
Brenden Lawlor <lawlorbrenden2@gmail.com>
2025-10-22 20:21:35 -0600

    feat: implement Authenticated component and integrate with Login for user authentication

---
+++ src/login/authenticated.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useNavigate }  from 'react-router-dom';
+
+import Button from 'react-bootstrap/Button';
+
+export function Authenticated(props) {
+  const navigate = useNavigate();
+
+  function logout() {
+    localStorage.removeItem('userName');
+    props.onLogout();
+  };
+
+  return (
+    <div>
+      <div className='userNameDisplay'>{props.userName}</div>
+      <Button className="btn btn-primary" variant='primary' onClick={() => navigate('/progress')}>
+        Begin
+      </Button>
+      <Button className="btn btn-dark" variant='secondary' onClick={() => logout()}>
+        Sign out
+      </Button>
+    </div>
+  );
+}
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
+
 export function Login({ userName, authState, onAuthChange }) {
   return (
     <main className="container-fluid bg-dark text-light d-flex flex-column align-items-center mt-5 pt-3">
-      <div className="content-wrapper">
-        <h1>Welcome</h1>
-        <form method="get" action="/progress" className="text-center">
-          <div className="input-group mb-3">
-            <span className="input-group-text">@</span>
-            <input className="form-control" type="text" placeholder="your@email.com" />
-          </div>
-          <div className="input-group mb-3">
-            <span className="input-group-text">🔒</span>
-            <input className="form-control" type="password" placeholder="password" />
-          </div>
-          <button type="submit" className="btn btn-primary">Sign in</button>
-          <button type="submit" className="btn btn-dark">Create</button>
-        </form>
+      <div>
+        {authState !== AuthState.Unknown && <h1>Welcome</h1>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
       </div>
     </main>
   );
