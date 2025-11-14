8dafb93dbab58388a485f333d9d9125c3c8056cf
Brenden Lawlor <lawlorbrenden2@gmail.com>
2025-11-13 19:17:14 -0700

    added full database.js example

--- service/database.js
+++ service/database.js
@@ -17,6 +17,34 @@ async function main() {
     console.log(`Connection failed to ${url} because ${ex.message}`);
     process.exit(1);
   }
+
+  try {
+    // Insert a document
+    const house = {
+      name: 'Beachfront views',
+      summary: 'From your bedroom to the beach, no shoes required',
+      property_type: 'Condo',
+      beds: 1,
+    };
+    await collection.insertOne(house);
+
+    // Query the documents
+    const query = { property_type: 'Condo', beds: { $lt: 2 } };
+    const options = {
+      sort: { name: -1 },
+      limit: 10,
+    };
+    const cursor = collection.find(query, options);
+    const rentals = await cursor.toArray();
+    rentals.forEach((i) => console.log(i));
+
+    // Delete documents
+    await collection.deleteMany(query);
+  } catch (ex) {
+    console.log(`Database (${url}) error: ${ex.message}`);
+  } finally {
+    await client.close();
+  }
 }
 
 main();
