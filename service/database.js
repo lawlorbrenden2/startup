5aed36ce95270aad7a908abb6dd03d9da19ee69a
Brenden Lawlor <lawlorbrenden2@gmail.com>
2025-11-13 19:40:01 -0700

    added functions and module.exports to database.js

--- service/database.js
+++ service/database.js
@@ -36,3 +36,40 @@ async function addUser(user) {
 async function updateUser(user) {
   await userCollection.updateOne({ email: user.email }, { $set: user });
 }
+
+function getWorkouts(email) {
+    return workoutCollection.find({ userEmail: email }).toArray();
+}
+
+async function saveWorkout(workout) {
+  await workoutCollection.updateOne(
+    { userEmail: workout.userEmail, day: workout.day },
+    { $set: workout },
+    { upsert: true }
+  );
+}
+
+function getFriends(email) {
+    return friendCollection.find({ userEmail: email }).toArray();
+}
+
+async function addFriend(friend) {
+    await friendCollection.insertOne(friend);
+}
+
+function getReactions(email) {
+    return reactionCollection.find({ userEmail: email }).toArray();
+}
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  addUser,
+  updateUser,
+  getWorkouts,
+  saveWorkout,
+  getFriends,
+  addFriend,
+  getReactions,
+  addReaction,
+};
