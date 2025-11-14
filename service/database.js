5024fa0935656777f9866ea5c1f548cb3add2106
Brenden Lawlor <lawlorbrenden2@gmail.com>
2025-11-13 19:34:04 -0700

    added more database collections

--- service/database.js
+++ service/database.js
@@ -5,46 +5,34 @@ const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostna
 
 // Connect to the database cluster
 const client = new MongoClient(url);
-const db = client.db('rental');
-const collection = db.collection('house');
+const db = client.db('flexbook');
+const userCollection = db.collection('users');
+const workoutCollection = db.collection('workouts');
+const friendCollection = db.collection('friends');
+const reactionCollection = db.collection('reactions');
 
-async function main() {
+(async function testConnection() {
   try {
-    // Test that you can connect to the database
     await db.command({ ping: 1 });
-    console.log(`DB connected to ${config.hostname}`);
+    console.log(`Connect to database`);
   } catch (ex) {
-    console.log(`Connection failed to ${url} because ${ex.message}`);
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
     process.exit(1);
   }
+})();
 
-  try {
-    // Insert a document
-    const house = {
-      name: 'Beachfront views',
-      summary: 'From your bedroom to the beach, no shoes required',
-      property_type: 'Condo',
-      beds: 1,
-    };
-    await collection.insertOne(house);
-
-    // Query the documents
-    const query = { property_type: 'Condo', beds: { $lt: 2 } };
-    const options = {
-      sort: { name: -1 },
-      limit: 10,
-    };
-    const cursor = collection.find(query, options);
-    const rentals = await cursor.toArray();
-    rentals.forEach((i) => console.log(i));
-
-    // Delete documents
-    await collection.deleteMany(query);
-  } catch (ex) {
-    console.log(`Database (${url}) error: ${ex.message}`);
-  } finally {
-    await client.close();
-  }
+function getUser(email) {
+  return userCollection.findOne({ email: email });
 }
 
-main();
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ email: user.email }, { $set: user });
+}
