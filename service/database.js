7d5d37158729b230609c439e2a3cbf069ff2e1bf
Brenden Lawlor <lawlorbrenden2@gmail.com>
2025-11-13 19:12:08 -0700

    added database.js

---
+++ service/database.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+
+// Connect to the database cluster
+const client = new MongoClient(url);
+const db = client.db('rental');
+const collection = db.collection('house');
+
+async function main() {
+  try {
+    // Test that you can connect to the database
+    await db.command({ ping: 1 });
+    console.log(`DB connected to ${config.hostname}`);
+  } catch (ex) {
+    console.log(`Connection failed to ${url} because ${ex.message}`);
+    process.exit(1);
+  }
